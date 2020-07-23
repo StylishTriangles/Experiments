bda046c252db28934f4972fa25a67f4179c43c20
StylishTriangles <hungrycow2012@gmail.com>
2020-07-23 21:26:24 +0200

    Fixed a bug where tests would overwrite testing datasets

--- branchless/benchmark.cpp
+++ branchless/benchmark.cpp
@@ -2,6 +2,7 @@
 #include <timer.hpp>
 #include <cstdio>
 #include <chrono>
+#include <cstring>
 #include "branchless.hpp"
 
 const int t_size = 100000;
@@ -9,12 +10,13 @@ const int t_count = 1000;
 
 using time_unit = std::chrono::microseconds;
 
-void benchmark(std::string dataset[], void (*func) (std::string&), const std::string& dataset_name, const std::string& func_name) {
+void benchmark(const std::string dataset[], void (*func) (std::string&), const std::string& dataset_name, const std::string& func_name) {
     uint64_t min = -1, max = 0, sum = 0;
     for (int i = 0; i < t_count; i++) {
+        std::string tested = dataset[i];
         Timer::start();
         // call the tested function
-        func(dataset[i]);
+        func(tested);
 
         auto t = Timer::stop();
         auto t_conv = std::chrono::duration_cast<time_unit>(t);
@@ -28,12 +30,14 @@ void benchmark(std::string dataset[], void (*func) (std::string&), const std::st
     printf("%s(set %s): %luus(min) %luus(max) %luus(avg)\n", func_name.c_str(), dataset_name.c_str(), min, max, sum/t_count);
 }
 
-void benchmark_C(char* dataset[], void (*func) (char*, int), const std::string& dataset_name, const std::string& func_name) {
+void benchmark_C(const char* dataset[], void (*func) (char*, int), const std::string& dataset_name, const std::string& func_name) {
     uint64_t min = -1, max = 0, sum = 0;
     for (int i = 0; i < t_count; i++) {
+        char* tested = new char[t_size];
+        memcpy(tested, dataset[i], t_size);
         Timer::start();
         // call the tested function
-        func(dataset[i], t_size);
+        func(tested, t_size);
 
         auto t = Timer::stop();
         auto t_conv = std::chrono::duration_cast<time_unit>(t);
@@ -43,6 +47,7 @@ void benchmark_C(char* dataset[], void (*func) (char*, int), const std::string& 
         if (count < min)
             min = count;
         sum += count;
+        delete[] tested;
     }
     printf("%s(set %s): %luus(min) %luus(max) %luus(avg)\n", func_name.c_str(), dataset_name.c_str(), min, max, sum/t_count);
 }
@@ -52,9 +57,9 @@ int main() {
     std::string b[t_count];
     std::string c[t_count];
 
-    char* a_c[t_count];
-    char* b_c[t_count];
-    char* c_c[t_count];
+    const char* a_c[t_count];
+    const char* b_c[t_count];
+    const char* c_c[t_count];
 
     printf("Characters in string: %d, string count per test: %d\n\n", t_size, t_count);
     printf("Generating random datasets... ");
@@ -65,9 +70,9 @@ int main() {
         c[i] = random_string(t_size); // few affected
 
         // this is probably illegal in some countries
-        a_c[i] = &a[i][0];
-        b_c[i] = &b[i][0];
-        c_c[i] = &c[i][0];
+        a_c[i] = a[i].c_str();
+        b_c[i] = b[i].c_str();
+        c_c[i] = c[i].c_str();
     }
     printf("done\n");
 
